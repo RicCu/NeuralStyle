108b6e6f7dea1716253bfbd3f25d61d583f5b978
RicCu <ricardocuenk@gmail.com>
2018-07-26 22:47:45 -0500

    Fix tensor creation in descriptive method

--- descriptive.py
+++ descriptive.py
@@ -35,20 +35,21 @@ def train(params):
     content_img = utils.load_image(params.content_data,
                                    params.shape).to(params.device)
     style_img = utils.load_image(params.style_data).to(params.device)
-    image = torch.empty(1, 3, *params.shape, device=params.device,
-                        requires_grad=True).uniform_()
+    image = torch.empty(1, 3, *params.shape,
+                        device=params.device).uniform_().requires_grad_()
     vgg = VGG16(STYLE_LAYERS).to(params.device)
     vgg.eval()
 
     # Prepare loss function and optimizer
     style_activations = vgg(style_img)
+    content_activations = vgg(content_img)
     loss_fn = PerceptualLoss(style_activations, STYLE_LAYERS, CONTENT_LAYERS,
                              params.style_weight, params.content_weight,
                              params.tv_weight).to(params.device)
-    del style_img
-    del style_activations
     optimizer = optim.LBFGS([image])
-    content_activations = vgg(content_img)
+    del style_img
+    del content_img
+    del style_activations
 
     # Store statistics
     style_score = list()
@@ -72,9 +73,9 @@ def train(params):
 
         # Log avg. losses (through epochs AND LBFGS iterations)
         if epoch % params.log_every == 0:
-            style_score = torch.Tensor(style_score).mean().unsqueeze(0)
-            content_score = torch.Tensor(content_score).mean().unsqueeze(0)
-            tv_score = torch.Tensor(tv_score).mean().unsqueeze(0)
+            style_score = torch.tensor(style_score).mean().unsqueeze(0)
+            content_score = torch.tensor(content_score).mean().unsqueeze(0)
+            tv_score = torch.tensor(tv_score).mean().unsqueeze(0)
             total_loss_graph.add(epoch, (style_score + content_score +
                                          tv_score).detach())
             losses_graph.add(epoch, style_score.detach(), STYLE_LOSS_TRACE)
