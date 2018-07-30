17b1cad175bf0fd93dd46af0b11e008126c878d6
RicCu <ricardocuenk@gmail.com>
2018-07-29 22:46:45 -0500

    Add ONNX export utility

---
+++ export.py
@@ -0,0 +1,55 @@
+"""Export pretrained models into ONNX format"""
+
+import argparse
+import os
+
+import torch
+
+import utils
+from model import FastStyle
+
+
+INPUT_NAME = ['original',]
+OUTPUT_NAME = ['transformed',]
+
+def main():
+    # Argument parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', type=str, default=None,
+                        help='Dummy image to use as input during export. '
+                        'Leave empty to use a random tensor.')
+    parser.add_argument('--ckpt', type=str, default=None,
+                        help='Path to model checkpoint. Must include the '
+                        'directory that contains the file, it will be used '
+                        'as the export\'s name')
+    parser.add_argument('--shape', nargs='+', type=int,
+                        help='Height x width')
+    params = parser.parse_args()
+
+    assert (params.data is not None or params.shape is not None), ('Set at '
+                                                               'least shape '
+                                                               'or data')
+    if params.shape is not None:
+        assert len(params.shape) == 2, 'Shape must include height and width'
+    assert params.ckpt is not None, 'Path to checkpoint file must be provided'
+    assert '/' in params.ckpt, ('Checkpoint path must include at least the '
+                                'parent directory of the file')
+    if params.data is None:
+        data = torch.randn(1, 3, *params.shape)
+    else:
+        data = utils.load_image(params.data, params.shape)
+    model = FastStyle()
+
+    # Get model name from checkpoint directory
+    model_name = params.ckpt.split('/')[-2]
+
+    utils.maybe_create_dir('onnx')
+    onnx_path = os.path.join('onnx', model_name + '.onnx')
+    model.load_state_dict(torch.load(params.ckpt))
+    torch.onnx.export(model, data, onnx_path, input_names=INPUT_NAME,
+                      output_names=OUTPUT_NAME, verbose=False)
+
+
+if __name__=='__main__':
+    main()
+
